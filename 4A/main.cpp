8df94c1d3c1ec3830c11ecac58bc403c82cfd67f
Smin Rana <sminrana@gmail.com>
2021-12-27 23:32:08 +0600

    Added 4A project

---
+++ 4A/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  4A
+//
+//  Created by sminrana@outlook.com.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int w;
+
+    cin >> w;
+
+    if (w != 2 && w % 2 == 0) {
+        cout << "YES" << "\n";
+    } else {
+        cout << "NO" << "\n";
+    }
+
+    return 0;
+}

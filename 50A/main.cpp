378627a1ff77404ce2497562cf5c8e21d42ba772
Smin Rana <sminrana@gmail.com>
2021-12-28 21:08:32 +0600

    Added 50A

---
+++ 50A/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int m=0,n=0;
+    int p=2;
+
+    cin>>m>>n;
+
+    int grid = m * n;
+    int pg = grid / p;
+    cout<<pg;
+
+    return 0;
+}

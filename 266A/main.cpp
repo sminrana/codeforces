9047bf5ad172a02b618da0bb625dd75cd7591ed7
Smin Rana <sminrana@gmail.com>
2021-12-31 21:20:40 +0600

    Added 266A and 281A

---
+++ 266A/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n,c=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for (int i=0; i<n-1; i++)
+        if (s[i]==s[i+1])
+            c++;
+    cout<<c;
+
+    return 0;
+}

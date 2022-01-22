979bd9171fbffdb428e4972769baf0b33ed33b33
Smin Rana <sminrana@gmail.com>
2022-01-22 13:55:02 +0600

    Added Queue at the School

---
+++ 266B/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x,y,i;string s;
+    cin>>x>>y>>s;
+
+    while(y--)
+        for(i=0;i<s.size()-1;i++)
+            if(s[i]=='B'&&s[i+1]=='G')
+                swap(s[i],s[i+1]),i++;
+    cout<<s;
+
+    return 0;
+}
